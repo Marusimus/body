9e89038f0065cac56233afa18abaa369df08cdfe
Marusimus <personal.wiches666@gmail.com>
2025-02-27 22:19:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int BMI = service.calculate(1.85, 98);
-        System.out.println("BMI-индекс= " + BMI);
+        int bmi = service.calculate(1.85, 98);
+        System.out.println("BMI-индекс= " + bmi);
     }
 }
